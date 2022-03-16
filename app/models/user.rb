4e0990dc6103136fa403dbf7aadf2a4663867c3e
ryosuke3455 <ryosuke.m2ds7@softbank.ne.jp>
2022-03-16 19:14:39 +0900

    テストコードの調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true, message: 'には6文字以上で、英字と数字の両方を含めて設定してください'
+  validates_format_of :password_confirmation, with: PASSWORD_REGEX, allow_blank: true, message: 'には6文字以上で、英字と数字の両方を含めて設定してください'
 end
