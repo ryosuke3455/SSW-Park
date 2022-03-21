b0e026009c6ad52b581de28730f1d3162652c804
ryosuke3455 <ryosuke.m2ds7@softbank.ne.jp>
2022-03-21 15:01:17 +0900

    投稿機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,14 @@ class Post < ApplicationRecord
   validates :image, presence: true
   validates :title, presence: true
   validates :explanation, presence: true
-  validates :category_id, numericality: { other_than: 0}
-  validates :junre_id, numericality: { other_than: 0}
+  validates :category_id, numericality: { other_than: 0, message: 'を選択してください'}
+  validates :junre_id, numericality: { other_than: 0, message: 'を選択してください'}
   
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+  belongs_to :category
+
 end
